bd4a76b35609c94304ff3a4da0c4d73818e39eb7
Clairence <clairencelie@gmail.com>
2023-08-22 14:28:47 +0700

    Contoh library sederhana composer

---
+++ src/Customer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Clairence\Belajar;
+
+class Customer
+{
+
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name." . PHP_EOL;
+    }
+}
